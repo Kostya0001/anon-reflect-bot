5ae4f2c09fa81eb6cdb7dd43bde836ec6fa7f74d
tkach <tkach2380@gmail.com>
2025-04-03 14:35:39 +0300

    5

--- anon_bot.py
+++ anon_bot.py
@@ -138,6 +138,7 @@ async def new_round(context):
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_all_text))
     app.run_webhook(
         listen="0.0.0.0",
